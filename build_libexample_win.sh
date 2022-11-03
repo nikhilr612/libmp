84946f7fab15e5e7c85332dfa97a57157720c511
Raj Kundu <rjk368@gmail.com>
2022-11-03 09:27:58 -0400

    make slashes consistent

--- build_libexample_win.sh
+++ build_libexample_win.sh
@@ -1,7 +1,7 @@
 export BAZEL_VC='C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC'
 export BAZEL_VS='C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools'
 # Use double slashes
-export PYTHON_BIN_PATH='C:\\Users//rk274//.conda//envs//cpp_mp//python.exe'
+export PYTHON_BIN_PATH='C:\\Users\\rk274\\.conda\\envs\\cpp_mp\\python.exe'
 
 # Optional vars
 # export BAZEL_VC_FULL_VERSION="14.33.31629"
